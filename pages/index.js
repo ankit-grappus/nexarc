80a5a406673d60d563e1453219e82f6705fcade2
deepak_2019 <deepak.khandelwal@squareboat.com>
2021-09-16 20:07:05 +0530

    ADD: OG tags

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Helmet from '../components/Seo'
 import Header from '../components/header'
 import HeroSliderSec from '../components/heroSliderSec'
@@ -10,10 +11,31 @@ import Footer from '../components/footer'
 export default function App() {
   return (
     <div className="app-container">
-      <Helmet
-        title="Welcome to nexarc"
-        metaContent=""
-      />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Welcome to Nexarc</title>
+        <meta name="description" content="nexarc (a Tata enterprise) is a digital growth platform that enables emerging businesses to grow, solve challenges and optimize processes." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#823cff" />
+        <meta name="description"
+          content="nexarc (a Tata enterprise) is a digital growth platform that enables emerging businesses to grow, solve challenges and optimize processes." />
+        <meta name="keywords" content="" />
+        <meta name="theme-color" content="#823cff" />
+        <meta name="msapplication-navbutton-color" content="#823cff" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#823cff" />
+        <meta property="og:site_name" content="Nexarc" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Nexarc" />
+        <meta property="og:url" content="https://nexarc.grappus.sbtechzone.com/" />
+        <meta property="og:image" content="https://nexarc.grappus.sbtechzone.com/static/og-img.png" />
+        <meta property="og:image:url" content="https://nexarc.grappus.sbtechzone.com/static/og-img.png" />
+        <meta property="og:image:alt" content="Nexarc" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="images/png" />
+        <meta property="og:description"
+          content="nexarc (a Tata enterprise) is a digital growth platform that enables emerging businesses to grow, solve challenges and optimize processes." />
+      </Head>
 
       <Header />
       <HeroSliderSec />
