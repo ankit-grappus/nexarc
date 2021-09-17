44d3c543fd163eed491bacca95057355fe9309a9
deepak_2019 <deepak.khandelwal@squareboat.com>
2021-09-17 17:31:44 +0530

    FIX: feedback changes

--- components/heroSliderSec/sliderData.js
+++ components/heroSliderSec/sliderData.js
@@ -1,7 +1,7 @@
 import TataEnterpriseText from "../tataEnterpriseText";
 import PrimaryButton from "../button";
 
-// slider 1
+// slide 1
 export const Slider1Data = () => {
   return (
     <>
@@ -13,7 +13,7 @@ export const Slider1Data = () => {
   )
 }
 
-// slider 2
+// slide 2
 export const Slider2Data = () => {
   return (
     <>
@@ -31,6 +31,24 @@ export const Slider2Data = () => {
   )
 }
 
+// slide 3
+export const Slider3Data = () => {
+  return (
+    <>
+      <div className="slider-content slide-3">
+        <h3 className="heading md">Worried about document security?</h3>
+        <p className="description text-white">Store and share files securely with TCS ION Document manager</p>
+        <PrimaryButton
+          className="know-more-btn primary btn-lg"
+          buttonName="Request a demo"
+          btnRightIcon="../../static/assets/images/icons/icon-rt-arrow-white.svg"
+        />
+        <img className="slider-rt-image absolute" src="../../static/assets/images/data-protection.png" alt="lappy" width="647" height="414" />
+      </div>
+    </>
+  )
+}
+
 export const carouselItems = [
   {
     sliderContent: <Slider1Data />
@@ -39,6 +57,6 @@ export const carouselItems = [
     sliderContent: <Slider2Data />
   },
   {
-    sliderContent: <Slider1Data />
+    sliderContent: <Slider3Data />
   },
 ]
