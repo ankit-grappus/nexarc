b47e6db2b3398a0835cb6656a6e32e2b9fd32730
ayushdev07 <ayush@uniworksdesigns.com>
2021-10-06 19:19:58 +0530

    banner images

--- components/heroSliderSec/sliderData.js
+++ components/heroSliderSec/sliderData.js
@@ -5,9 +5,10 @@ import PrimaryButton from "../button";
 export const Slider1Data = () => {
   return (
     <>
-      <div className="slider-content slide-1 center">
-        <h3 className="heading mx-auto lg">nexarc helps you grow your business</h3>
-        <TataEnterpriseText />
+      <div className="slider-content slide-1">
+        {/* <h3 className="heading mx-auto lg">nexarc helps you grow your business</h3>
+        <TataEnterpriseText /> */}
+          <img className="slider-rt-image absolute" src="https://apps.tcsionhub.in/dotcom/staticpages/front-end/static-appcode/nexarc/homepage/images/marketing-banner/1st_bannerBigScreen_20210901_1855@2x.jpg" alt="lappy" width="100%" height="400px" />
       </div>
     </>
   )
@@ -18,14 +19,14 @@ export const Slider2Data = () => {
   return (
     <>
       <div className="slider-content slide-2">
-        <h3 className="heading md">Are you ready for E-invoicing*?</h3>
+        {/* <h3 className="heading md">Are you ready for E-invoicing*?</h3>
         <p className="description text-white">Try the simple & affordable E-invoicing solutions from TCS</p>
         <PrimaryButton
           className="know-more-btn primary btn-lg"
           buttonName="Know more"
           btnRightIcon="../../static/assets/images/icons/icon-rt-arrow-white.svg"
-        />
-        <img className="slider-rt-image absolute" src="../../static/assets/images/lappy-with-paper-plane.png" alt="lappy" width="647" height="414" />
+        /> */}
+        <img className="slider-rt-image absolute" src="https://apps.tcsionhub.in/dotcom/staticpages/front-end/static-appcode/nexarc/homepage/images/marketing-banner/3rd_bannerBigScreen_20210901_1855@2x.png" alt="lappy" width="100%" height="500px" />
       </div>
     </>
   )
@@ -36,14 +37,15 @@ export const Slider3Data = () => {
   return (
     <>
       <div className="slider-content slide-3">
-        <h3 className="heading md">Worried about document security?</h3>
-        <p className="description text-white">Store and share files securely with TCS ION Document manager</p>
-        <PrimaryButton
+        {/* <h3 className="heading md">Worried about document security?</h3>
+        <p className="description text-white">Store and share files securely with TCS ION Document manager</p> */}
+        {/* <PrimaryButton
           className="know-more-btn primary btn-lg"
           buttonName="Request a demo"
           btnRightIcon="../../static/assets/images/icons/icon-rt-arrow-white.svg"
-        />
-        <img className="slider-rt-image absolute" src="../../static/assets/images/data-protection.png" alt="lappy" width="443" height="296" />
+          width="100%" height="296"
+        /> */}
+        <img className="slider-rt-image absolute" src="https://apps.tcsionhub.in/dotcom/staticpages/front-end/static-appcode/nexarc/homepage/images/marketing-banner/2nd_bannerBigScreen_20210901_1855@2x.png" alt="lappy" width="100%" height="400px" />
       </div>
     </>
   )
