ac75c6322aa2c0ed32f295ce572862d62741f366
ayushdev07 <ayush@uniworksdesigns.com>
2021-11-16 10:57:31 +0530

    gtm tracker added

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,15 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* <!-- Google Tag Manager --> */}
+        <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NT8M3DJ');`,
+          }}>
+        </script>
         <link rel="icon" href="/favicon.ico" />
         <link rel="canonical" href="https://www.nexarc.in/" />
         <title>Welcome to Nexarc</title>
@@ -47,7 +56,7 @@ function MyApp({ Component, pageProps }) {
         {/* <!-- end twitter tags--> */}
 
         {/* <!-- Google Tag Manager --> */}
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
             function (w, d, s, l, i) {
@@ -60,7 +69,15 @@ function MyApp({ Component, pageProps }) {
             }(window, document, 'script', 'dataLayer', "GTM-NT8M3DJ")
           `
           }}
-        />
+        /> */}
+        {/* <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NT8M3DJ');`,
+          }}>
+        </script> */}
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,15 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          {/* <!-- Google Tag Manager --> */}
+        <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NT8M3DJ');`,
+          }}>
+        </script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -48,27 +57,16 @@ class MyDocument extends Document {
                   }) 
             }}
         />
-            <script dangerouslySetInnerHTML={{ __html: `
-                function (w, d, s, l, i) {
-                  w[l] = w[l] || []; w[l].push({
-                    'gtm.start':
-                      new Date().getTime(), event: 'gtm.js'
-                  }); var f = d.getElementsByTagName(s)[0],
-                    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                      'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-                }(window, document, 'script', 'dataLayer', "GTM-NT8M3DJ")
-              `}}>
-
-            </script>
         </Head>
         <body>
-          <Main />
-          <NextScript />
           {/* Google Tag Manager (noscript) */}
           <noscript>
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NT8M3DJ" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
           </noscript>
           {/*  End Google Tag Manager (noscript) */}
+          <Main />
+          <NextScript />
+          
         </body>
       </Html >
     );
