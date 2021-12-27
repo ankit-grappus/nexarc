a9205871097a0b66f7d2903a9b87ef32dc4a6383
ayushdev07 <ayush@uniworksdesigns.com>
2021-12-27 15:24:51 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function App() {
           <div className="looking-for--sec">
             <div className="container-fluid custom">
               <LookingForSec
-                secHeading="What are you looking for?"
+                secHeading=" are you looking for?"
               />
             </div>
           </div>
