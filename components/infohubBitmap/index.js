89dc79c1af2633d8c2802012207ae9104e6f7f39
ayushdev07 <ayush@uniworksdesigns.com>
2022-01-20 15:00:54 +0530

    curated img added

--- components/infohubBitmap/index.js
+++ components/infohubBitmap/index.js
@@ -9,10 +9,20 @@ function InfohubBitmap(){
                     <h2 className="textHeading">nexarc Advisors</h2>
                     <p className="subHeading">Expert advice at your fingertips</p>
                 </div>
-                <div className="advivorSecText">
+                {/* <div className="advivorSecText">
                     <imp className="advivorSecTextImg" src="../../static/images/fi_check-circle.png" alt="text" />
+                </div> */}
+                <div className="curatedList">
+                    <div className="curatedListSec">
+                        <img className="curatedImg" src="../../static/assets/images/fi_check-circle.png" alt="text" />
+                        <p className="curatedText">Curated list of business advisors and subject matter experts</p>
+                    </div>
+                    <div className="curatedListSec">
+                        <img className="curatedImg" src="../../static/assets/images/fi_check-circle.png" alt="text" />
+                        <p className="curatedText">Curated list of business advisors and subject matter experts</p>
+                    </div>
                 </div>
-                <img className="bitmapImg" src="../../static/assets/images/bitmap.png" alt="arrow logo" width="137" height="33" />
+                {/* <img className="bitmapImg" src="../../static/assets/images/bitmap.png" alt="arrow logo" width="137" height="33" /> */}
                 
                 <div className="KnowMore">
                     <button type="button" className="btn">Know More</button>
