a5a56f24b9ae7cf518309e460ae2644bd7614860
ayushdev07 <ayush@uniworksdesigns.com>
2022-01-20 14:25:38 +0530

    new style changes for info hub

--- components/infohubBitmap/index.js
+++ components/infohubBitmap/index.js
@@ -4,6 +4,7 @@ import "./styles.scss"
 function InfohubBitmap(){
     return(
         <div className="bitmap">
+            <div className="bitLeftSide">
                 <div className="advisorSec">
                     <h2 className="textHeading">nexarc Advisors</h2>
                     <p className="subHeading">Expert advice at your fingertips</p>
@@ -11,15 +12,17 @@ function InfohubBitmap(){
                 <div className="advivorSecText">
                     <imp className="advivorSecTextImg" src="../../static/images/fi_check-circle.png" alt="text" />
                 </div>
-                
                 <img className="bitmapImg" src="../../static/assets/images/bitmap.png" alt="arrow logo" width="137" height="33" />
-                <div>
-                    <div className="left"></div>
-                    <img className="bitmapHuman" src="../../static/assets/images/bitmapHuman.png" alt="arrow logo" width="137" height="33" />
-                </div>
+                
                 <div className="KnowMore">
                     <button type="button" className="btn">Know More</button>
                     <img className="logo" src="../../static/assets/images/arrowR.png" alt="arrow logo" width="137" height="33" />
+                </div>
+            </div>
+                
+                <div className="bitRightSide">
+                    <div className="left"></div>
+                    <img className="bitmapHuman" src="../../static/assets/images/bitmapHuman.png" alt="arrow logo" width="137" height="33" />
                 </div>
         </div>
     )
